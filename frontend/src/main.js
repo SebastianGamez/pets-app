79cb64de5312f835050dbd487bbb189a3f27e56f
Sebastian Gamez Ariza <juan.gamez1001@gmail.com>
2023-03-09 13:35:23 -0500

    Creación del rutero

---
+++ frontend/src/main.js
@@ -0,0 +1,20 @@
+// Import the createApp function
+import { createApp } from 'vue'
+import App from './App.vue'
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+// Create routes
+const routes = [
+//   { path: '/', component: HomePage },
+]
+// Create the router
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+// Create the app
+const app = createApp(App)
+// Mount the router
+app.use(router)
+// Mount the app
+app.mount('#app')
